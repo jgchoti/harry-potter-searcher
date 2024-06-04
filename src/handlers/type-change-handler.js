bb34cc9ecb50e385a79faf19d514bfc1d6305bff
jgchoti <jgchotirat@gmail.com>
2024-06-04 21:11:52 +0200

    Create type-change-handler.js

---
+++ src/handlers/type-change-handler.js
@@ -0,0 +1,18 @@
+import { state } from "../../data/state.js";
+import { renderFilter } from "../components/render-filter.js";
+import { updateFilters } from "../utils/update-filters.js";
+import { updateSort } from "../utils/update-sort.js";
+
+export const typeChangeHandler = (searchTerm) => {
+    state.type = document.getElementById('types').value;
+
+    state.filter = [];
+    state.filterCount = 1;
+
+    updateSort()
+    updateFilters()
+
+    const filterContainer = document.getElementById('filter');
+    filterContainer.innerHTML = '';
+    renderFilter()
+};
