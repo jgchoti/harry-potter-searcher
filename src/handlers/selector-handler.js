8989a576682892106c9bdf22eda04f66b411dc72
jgchoti <jgchotirat@gmail.com>
2024-06-04 21:11:59 +0200

    Update selector-handler.js

--- src/handlers/selector-handler.js
+++ src/handlers/selector-handler.js
@@ -1,22 +1,22 @@
-import { state } from "../../data/state.js"
-import { setData } from "../utils/set-data.js"
-export const selectorHandler = (event) => {
-    const id = event.target.id
-    if (id === 'types') {
-        state.type = event.target.value
-        if (['characters', 'potions', 'spells'].includes(state.type)) {
-            state.filter.nameType = 'name';
-        } else {
-            state.filter.nameType = 'title';
-        }
-    } else if (id === 'filter') {
-        state.filter.filterBy = event.target.value
-    } else if (id === 'sort') {
-        state.sort.sortBy = event.target.value
-    }
-    else {
-        state.pagination.pageSize = Number(event.target.value)
-    }
-    setData()
-}
+import { state } from "../../data/state.js";
+import { setData } from "../utils/set-data.js";
+import { updateFilters } from "../utils/update-filters.js";
+import { updatePageSize } from "../utils/update-page-size.js";
+import { updateSort } from "../utils/update-sort.js";
+import { typeChangeHandler } from "./type-change-handler.js";
 
+export const selectorHandler = (event) => {
+    const outputContainer = document.getElementById('output');
+    outputContainer.innerHTML = '';
+    const id = event.target.id;
+
+    if (id === 'types') {
+        typeChangeHandler();
+    } else if (id === 'selector - page - size') {
+        updatePageSize(Number(event.target.value));
+    } else {
+        updateFilters();
+        updateSort();
+    }
+    setData();
+};
