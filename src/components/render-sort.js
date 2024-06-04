65e39304bfeb0cc40f10f1aa584c4ae563225f94
jgchoti <jgchotirat@gmail.com>
2024-06-04 21:11:42 +0200

    Update render-sort.js

--- src/components/render-sort.js
+++ src/components/render-sort.js
@@ -1,57 +1,20 @@
 import { state } from "../../data/state.js"
+import { FILTER_OPTION } from "../../data/constant.js"
 import { selectorListener } from "../listeners/selector-listener.js"
 import { sortBtnListener } from "../listeners/sort-btn-listener.js"
-export const renderSort = () => {
-    const outputContainer = document.getElementById('output')
-    const sortContainer = document.createElement('div')
-    sortContainer.id = ' sort-container'
-    outputContainer.appendChild(sortContainer)
+import { renderSelector } from "./render-selector.js"
 
-    const SORT_OPTION = {
-        'books': {
-            'title': 'Title',
-            'pages': 'Page',
-            'release_date': 'Release Date',
-        },
-        'characters': {
-            'name': 'Name',
-            'house': 'House',
-            'nationality': 'Nationality',
-            'alias_names': 'Alias Names',
-        },
-        'movies': {
-            'title': 'Title',
-            'release_date': 'Release Date',
-            'running_time': 'Running Time'
-        },
-        'potions': {
-            'name': 'Name',
-            'ingredients': 'ingredients',
-            'effect': 'Effect'
-        },
-        'spells': {
-            'name': 'Name',
-            'category': 'Category',
-            'light': 'Light',
-        }
-    }
-    const id = 'sort'
+export const renderSort = () => {
+    const sortContainer = document.getElementById('sort')
+    sortContainer.innerHTML = ""
+
+    const id = 'selector-sort'
     const labelEl = document.createElement('label')
     labelEl.htmlFor = id
     labelEl.innerText = 'Sort By'
     sortContainer.appendChild(labelEl)
 
-    const selectEl = document.createElement('select')
-    selectEl.id = id
-    for (const key in SORT_OPTION[state.type]) {
-        const optionEl = document.createElement('option')
-        optionEl.value = key
-        optionEl.innerText = SORT_OPTION[state.type][key]
-        if (key === state.sort.sortBy) {
-            optionEl.selected = true
-        }
-        selectEl.appendChild(optionEl)
-    }
+    const selectEl = renderSelector(id, FILTER_OPTION[state.type])
     sortContainer.appendChild(selectEl)
 
     const SORT_ORDER_BTN = ['ascending', 'descending']
