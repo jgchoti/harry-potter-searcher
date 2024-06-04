9c0209c5239b96a7bd4664926d561210b38dc6cd
jgchoti <jgchotirat@gmail.com>
2024-06-04 21:35:58 +0200

    Update update-page-size.js

--- src/utils/update-page-size.js
+++ src/utils/update-page-size.js
@@ -1,5 +1,10 @@
-export const updatePageSize = (value) => {
+import { state } from "../../data/state.js";
+import { renderPaginator } from "../components/render-paginator.js";
+
+export const updatePageSize = (value = 5) => {
+
     state.pagination.pageSize = Number(value);
-    updateFilters();
-    updateSort();
+    console.log(state.pagination.pageSize)
+    state.pagination.currentPage = 1
+
 };
