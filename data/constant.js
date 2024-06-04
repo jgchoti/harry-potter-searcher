0c09b40d8126125939df0191890fe9a567531711
jgchoti <jgchotirat@gmail.com>
2024-06-04 21:12:21 +0200

    Update constant.js

--- data/constant.js
+++ data/constant.js
@@ -4,3 +4,79 @@ export const BTN_PAGINATOR = {
     'next': '>',
     'last': 'Last',
 }
+
+export const FILTER_OPTION = {
+    'books': {
+        'title': 'Title',
+        'pages': 'Page',
+        'release_date': 'Release Date',
+        'author': 'Author',
+        'gender': 'Gender'
+    },
+    'characters': {
+        'name': 'Name',
+        'house': 'House',
+        'nationality': 'Nationality',
+        'alias_names': 'Alias Names',
+        'jobs': 'Jobs',
+        'family_member': 'family_member',
+        'romances': 'Romances',
+        'patronus': 'Patronus',
+        'wand': 'Wand',
+        'weight': 'Weight',
+        'height': 'Height',
+        'born': 'Born',
+        'died': 'Died',
+        'species': "Species",
+        'animagus': 'Animagus',
+        'boggart': 'Boggart',
+        'blood_status': 'Blood Status',
+        'skin_color': 'Skin Color',
+        'eye_color': 'Eye Color',
+    },
+    'movies': {
+        'title': 'Title',
+        'release_date': 'Release Date',
+        'running_time': 'Running Time',
+        'rating': 'Rating',
+        'box_office': 'Box Office',
+        'budget': 'Budget',
+        'cinematographers': 'Cinematographers',
+        'directors': 'Directors',
+        'producers': 'Producers',
+        'screenwriters': 'Screenwriters',
+        'music_composers': 'Music Composers',
+        'editors': 'Editors',
+        'distributors': 'Distributors'
+    },
+    'potions': {
+        'name': 'Name',
+        'ingredients': 'Ingredients',
+        'effect': 'Effect',
+        'characteristics': 'Characteristics',
+        'difficulty': 'Difficulty',
+        'inventors': 'Inventors',
+        'side_effects': 'Side Effects',
+        'time': 'Time'
+    },
+    'spells': {
+        'name': 'Name',
+        'category': 'Category',
+        'light': 'Light',
+        'creator': 'Creator',
+        'effect': 'Effect',
+        'hand': 'Hand',
+        'incantation': 'Incantation',
+    }
+}
+
+export const FILTER_BY_OPTION = {
+    'cont': 'contains',
+    'not_cont': "doesn't contain",
+    'eq': 'equals',
+    'not_eq': 'not equal to',
+    "start": 'starts with',
+    "end": 'ends with',
+    'lt': 'less than',
+    'gt': 'greater than'
+}
