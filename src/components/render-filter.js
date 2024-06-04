cb5594c5b8e37bab11b24760f8ff9956a5655eb7
jgchoti <jgchotirat@gmail.com>
2024-06-04 22:10:20 +0200

    Update render-filter.js

--- src/components/render-filter.js
+++ src/components/render-filter.js
@@ -8,42 +8,37 @@ import { textInputListener } from "../listeners/text-input-listener.js";
 export const renderFilter = () => {
     const container = document.getElementById('filter');
 
-    let addFilterBtn = document.getElementById('btn-add-filter');
-    if (!addFilterBtn) {
-        addFilterBtn = document.createElement('button');
-        addFilterBtn.innerHTML = 'Add Filter';
-        addFilterBtn.id = 'btn-add-filter';
-        container.appendChild(addFilterBtn);
-        addFilterListener(addFilterBtn.id);
-    }
-
-
     const filterId = `selector-filter-${state.filterCount}`;
     const filterById = `selector-filter-by-${state.filterCount}`;
     const inputId = `input-text-${state.filterCount}`;
 
-    const filterSelector = renderSelector(filterId, FILTER_OPTION[state.type]);
-    container.appendChild(filterSelector);
+    let btnContainer = document.getElementById('container-btn-add');
+    if (!btnContainer) {
+        btnContainer = document.createElement('div');
+        btnContainer.id = 'container-btn-add';
+        const addFilterBtn = document.createElement('button');
+        addFilterBtn.innerHTML = 'Add Filter';
+        addFilterBtn.id = 'btn-add-filter';
+        btnContainer.appendChild(addFilterBtn);
+        container.appendChild(btnContainer);
+        addFilterListener(addFilterBtn.id);
+    }
 
+    const filterSelector = renderSelector(filterId, FILTER_OPTION[state.type]);
+    container.insertBefore(filterSelector, btnContainer);
 
     const filterBySelector = renderSelector(filterById, FILTER_BY_OPTION);
-    container.appendChild(filterBySelector);
-
+    container.insertBefore(filterBySelector, btnContainer);
 
     const inputEl = document.createElement('input');
     inputEl.id = inputId;
     inputEl.type = "text";
     inputEl.className = "input-field";
-    container.appendChild(inputEl);
-
-    // Attach event listeners to the new elements
+    container.insertBefore(inputEl, btnContainer);
 
     textInputListener(inputId);
     selectorListener(filterId);
     selectorListener(filterById);
 
 };
-
-
-
 
