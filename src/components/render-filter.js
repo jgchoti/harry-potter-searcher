cf0ce269cb4b1dc8658c37876292a215c3bace37
jgchoti <jgchotirat@gmail.com>
2024-06-05 12:15:36 +0200

    Update render-filter.js

--- src/components/render-filter.js
+++ src/components/render-filter.js
@@ -2,7 +2,6 @@ import { state } from "../../data/state.js";
 import { FILTER_OPTION, FILTER_BY_OPTION } from "../../data/constant.js";
 import { selectorListener } from "../listeners/selector-listener.js";
 import { renderSelector } from "./render-selector.js";
-import { addFilterListener } from "../listeners/add-filter-listener.js";
 import { textInputListener } from "../listeners/text-input-listener.js";
 
 export const renderFilter = () => {
@@ -12,29 +11,22 @@ export const renderFilter = () => {
     const filterById = `selector-filter-by-${state.filterCount}`;
     const inputId = `input-text-${state.filterCount}`;
 
-    let btnContainer = document.getElementById('container-btn-add');
-    if (!btnContainer) {
-        btnContainer = document.createElement('div');
-        btnContainer.id = 'container-btn-add';
-        const addFilterBtn = document.createElement('button');
-        addFilterBtn.innerHTML = 'Add Filter';
-        addFilterBtn.id = 'btn-add-filter';
-        btnContainer.appendChild(addFilterBtn);
-        container.appendChild(btnContainer);
-        addFilterListener(addFilterBtn.id);
-    }
+    const filterContainer = document.createElement('div');
+    filterContainer.classList.add('container-filter')
 
     const filterSelector = renderSelector(filterId, FILTER_OPTION[state.type]);
-    container.insertBefore(filterSelector, btnContainer);
+    filterContainer.appendChild(filterSelector);
 
     const filterBySelector = renderSelector(filterById, FILTER_BY_OPTION);
-    container.insertBefore(filterBySelector, btnContainer);
+    filterContainer.appendChild(filterBySelector);
 
     const inputEl = document.createElement('input');
     inputEl.id = inputId;
     inputEl.type = "text";
     inputEl.className = "input-field";
-    container.insertBefore(inputEl, btnContainer);
+    filterContainer.appendChild(inputEl);
+
+    container.appendChild(filterContainer)
 
     textInputListener(inputId);
     selectorListener(filterId);
