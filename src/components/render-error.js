1577e14614edc1daff6a10d0e7c11fcb0e6c7bbf
jgchoti <jgchotirat@gmail.com>
2024-06-05 12:15:38 +0200

    Update render-error.js

--- src/components/render-error.js
+++ src/components/render-error.js
@@ -1,6 +1,7 @@
 export const renderError = (message) => {
     const outputContainer = document.getElementById('output')
     const pEl = document.createElement('p')
+    pEl.className.add('error-message')
     pEl.innerText = message
     outputContainer.appendChild(pEl)
 }
