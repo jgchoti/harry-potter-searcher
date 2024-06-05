89d023faf4e5378d873963dd5ef04d1e3486ec87
jgchoti <jgchotirat@gmail.com>
2024-06-05 12:15:20 +0200

    Update init.js

--- src/init.js
+++ src/init.js
@@ -1,8 +1,10 @@
+import { addFilterListener } from "./listeners/add-filter-listener.js";
 import { btnListener } from "./listeners/btn-listener.js";
 import { createFilterListener } from "./listeners/load-listener.js";
 import { selectorListener } from "./listeners/selector-listener.js";
 import { textInputListener } from "./listeners/text-input-listener.js";
 
 createFilterListener()
-btnListener()
-selectorListener('types')
+btnListener('btn-submit')
+addFilterListener('btn-add-filter')
+selectorListener('selector-types')
