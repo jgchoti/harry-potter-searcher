9f1aa0235f7b5c6d5c4c43379d1e84247a7c8a98
jgchoti <jgchotirat@gmail.com>
2024-06-05 12:15:30 +0200

    Update paginator-handler.js

--- src/handlers/paginator-handler.js
+++ src/handlers/paginator-handler.js
@@ -4,11 +4,11 @@ import { setData } from "../utils/set-data.js"
 export const paginatorHandler = (event) => {
     const pageSelect = event.target.id
     let newPage = state.pagination.currentPage
-    if (pageSelect === 'first') {
+    if (pageSelect.startsWith('first')) {
         newPage = 1
-    } else if (pageSelect === 'last') {
+    } else if (pageSelect.startsWith('last')) {
         newPage = state.pagination.totalPages
-    } else if (pageSelect === 'next') {
+    } else if (pageSelect.startsWith('next')) {
         newPage = Math.min(newPage + 1, state.pagination.totalPages)
     } else {
         newPage = Math.max(newPage - 1, 1)
