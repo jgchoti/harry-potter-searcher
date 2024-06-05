50f5ff117a47a5496fd17622c73b82674597849d
jgchoti <jgchotirat@gmail.com>
2024-06-05 12:15:34 +0200

    Update render-page-result.js

--- src/components/render-page-result.js
+++ src/components/render-page-result.js
@@ -2,15 +2,17 @@ import { state } from "../../data/state.js"
 
 export const renderPageResult = () => {
     const outputContainer = document.getElementById('output')
+    const searchResultContainer = document.createElement('div')
+    searchResultContainer.id = 'container-search-result'
 
     state.data.map(item => {
         const container = document.createElement('div')
-
+        container.classList.add('item-card')
         const titleEl = document.createElement('h3')
         titleEl.textContent = `${state.type === "characters" || state.type === 'spells' || state.type === 'potions' ? item.attributes.name : item.attributes.title}`
 
         container.appendChild(titleEl)
-        outputContainer.appendChild(container)
+        searchResultContainer.appendChild(container)
     })
-
+    outputContainer.appendChild(searchResultContainer)
 }
