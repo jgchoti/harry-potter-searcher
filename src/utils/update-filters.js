1f8d3c1309bf3bb6ffdd0af9471227d17f5c978e
pnchoti <jgchotirat@gmail.com>
2024-06-08 18:01:05 +0200

    Update update-filters.js

--- src/utils/update-filters.js
+++ src/utils/update-filters.js
@@ -1,23 +1,40 @@
 import { state } from "../../data/state.js";
 
-export const updateFilters = () => {
-    const elements = document.querySelectorAll('input');
-    state.filterCount = elements.length;
-    state.filter = []
+export const updateFilters = (type = '') => {
+    state.filter = [];
+    if (type === 'reset') {
+        state.filterCount = 1;
+        let filter = {
+            filterType: state.type === 'characters' || state.type === 'potions' || state.type === 'spells' ? 'name' : 'title',
+            filterBy: 'cont',
+            searchTerm: ""
+        };
+        state.filter.push(filter);
+    } else {
+        for (let i = 1; i <= state.filterCount; i++) {
+            const selectorFilter = document.getElementById(`selector-filter-${i}`);
+            const selectorFilterBy = document.getElementById(`selector-filter-by-${i}`);
+            const searchTerm = document.getElementById(`input-text-${i}`);
 
-    for (let i = 0; i <= state.filterCount; i++) {
-        const selectorFilter = document.getElementById(`selector-filter-${i}`);
-        const selectorFilterBy = document.getElementById(`selector-filter-by-${i}`);
-        const searchTerm = document.getElementById(`input-text-${i}`);
+            if (selectorFilter && selectorFilterBy) {
+                let filter = {
+                    filterType: selectorFilter.value,
+                    filterBy: selectorFilterBy.value || 'cont',
+                    searchTerm: searchTerm ? searchTerm.value : ""
+                };
+                state.filter.push(filter);
+            }
+        }
 
-        console.log(selectorFilter)
-        if (selectorFilter && selectorFilterBy) {
-            let filter = {
-                filterType: selectorFilter.value,
-                filterBy: selectorFilterBy.value ? selectorFilterBy.value : 'cont',
-                searchTerm: searchTerm ? searchTerm.value : ""
+        if (state.filter.length === 0) {
+            let defaultFilter = {
+                filterType: state.type === 'characters' || state.type === 'potions' || state.type === 'spells' ? 'name' : 'title',
+                filterBy: 'cont',
+                searchTerm: ""
             };
-            state.filter.push(filter);
+            state.filter.push(defaultFilter);
+            state.filterCount = 1;
         }
     }
+    console.log(state);
 };
