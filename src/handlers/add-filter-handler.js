516e519df004162f363e98a6671af942ce4990c7
pnchoti <jgchotirat@gmail.com>
2024-06-08 18:01:00 +0200

    Update add-filter-handler.js

--- src/handlers/add-filter-handler.js
+++ src/handlers/add-filter-handler.js
@@ -1,7 +1,18 @@
 import { state } from "../../data/state.js";
 import { renderFilter } from "../components/render-filter.js";
+import { updateFilters } from "../utils/update-filters.js";
 
-export const addFilterHandler = () => {
-    state.filterCount++
-    renderFilter()
+export const addFilterHandler = (event) => {
+    const id = event.target.id;
+    console.log(id);
+    if (id === 'btn-add-filter') {
+
+        state.filterCount++;
+        console.log(state.filterCount);
+        renderFilter();
+        updateFilters('add');
+    } else {
+        renderFilter(true);
+        updateFilters('reset');
+    }
 };
